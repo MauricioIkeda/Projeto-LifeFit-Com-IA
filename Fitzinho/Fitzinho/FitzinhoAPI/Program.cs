8eea299c25dd898311ce0a15c597ff1b2675618a
Vinicius-Brandi <vviniciusbrandi@gmail.com>
2025-11-22 16:22:25 -0300

    Configurando Program

--- Fitzinho/Fitzinho/FitzinhoAPI/Program.cs
+++ Fitzinho/Fitzinho/FitzinhoAPI/Program.cs
@@ -1,3 +1,7 @@
+using FGB.Api.Injecoes;
+using LifeFit.Dominio.Servicos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddFGB();
+builder.Services.AddAutoMapperProfiles();
+
+builder.Services.AddTransient<RequisicaoSugestaoServico>();
+builder.Services.AddTransient<PerfilUsuarioServico>();
+builder.Services.AddTransient<ExercicioServico>();
+builder.Services.AddTransient<FeedbackServico>();
+builder.Services.AddTransient<SugestaoServico>();
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
